4c5a5319655df8e64243aa534b57d2469306fda0
eperst <eper.st@gmail.com>
2023-11-12 13:41:04 +0100

    add test saliency function

--- prediction.py
+++ prediction.py
@@ -24,6 +24,25 @@ def extract_features(audio_path: str, model_path: str):
 
     return representation
 
+def saliency(x, model, mask_bs, inp_shape, num_batches, pdist, dev="cpu"):
+    h_star = model(x)
+    h_star = h_star.expand(mask_bs, -1)
+    saliency = torch.zeros((inp_shape, inp_shape), device=dev)
+    saliency_var = torch.zeros((inp_shape, inp_shape), device=dev)
+    for mask_idx, mask in enumerate(tqdm.tqdm(MaskGenerator(
+                                                num_batches, (inp_shape), mask_bs=mask_bs),
+                                                total=num_batches,
+                                                desc=f"Compute {model_name} importance")):
+
+          x_mask = x * mask
+          out = model(x_mask)
+
+          out = pdist(h_star, out)[:, None, None, None]
+
+          saliency += torch.mean(out * mask, dim=(0, 1))
+   
+    return saliency / (num_batches*0.5)
+
 if __name__ == '__main__':
     filename = '/zhome/58/f/181392/DTU/DL/Project/DL_RELAX/audio/sounds/1-9886-A-49.wav'
     model_path = '/zhome/58/f/181392/DTU/DL/Project/DL_RELAX/audio/models/BEATs_iter3_plus_AS2M_finetuned_on_AS2M_cpt2.pt'
