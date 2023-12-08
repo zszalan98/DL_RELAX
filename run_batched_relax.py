b1515c71414f60ae8241d22b12617a7c4c7f1b26
Zalan Zsiboras <zszalan98@gmail.com>
2023-12-08 13:13:50 +0100

    Float masks. Debug plotting  and file creation. New default settings in Fourier transforms.

--- run_batched_relax.py
+++ run_batched_relax.py
@@ -2,27 +2,30 @@
 import torch
 from tools.audio import prepare_audio, inverse_complex_spectrogram
 from tools.beats import load_beats_model
-from tools.masking import create_random_masks, apply_masks
+from tools.masking import create_random_masks, apply_masks, create_relax_masks, apply_advanced_masks
 from torch.nn.functional import cosine_similarity as cosine_sim
 from tools.batched_relax import update_importance, update_uncertainty, manhattan_similarity
-from tools.plotting import plot_results
+from tools.plotting import plot_results, plot_and_save_masks, plot_and_save_spec, plot_and_save_masked_audio
 from pathlib import Path
 import matplotlib.pyplot as plt
 
 
 # Settings classes
 class RelaxSettings:
-    num_of_batches: int = 100  # Number of batches
-    num_of_masks: int = 100  # Number of masks per batch
-    
+    def __init__(self):
+        self.num_of_batches = 10  # Number of batches
+        self.num_of_masks = 10  # Number of masks per batch
+
 class AudioSettings:
-    audio_filename: str = 'frog_1.wav'  # Audio filename
+    def __init__(self):
+        self.audio_filename = 'frog_1.wav'  # Audio filename
 
 class MaskingSettings:
-    seed: int = 42  # Random seed (needed due to batched processing)
-    n_freq: int = 40  # Number of frequency bins
-    n_time: int = 25  # Number of time bins
-    p: float = 0.6  # Bernoulli distribution parameter
+    def __init__(self):
+        self.n_freq = 20  # Number of frequency bins
+        self.n_time = 10  # Number of time bins
+        self.p = 0.5  # Bernoulli distribution parameter
+        self.seed = 42  # Random seed (needed due to batched processing)
 
 
 class AllSettings:
@@ -38,6 +41,9 @@ def run_batched_relax(home_path: Path, settings: AllSettings):
     ## Settings
     num_batches = settings.relax.num_of_batches
     num_masks = settings.relax.num_of_masks
+    # Debug files
+    debug_folder = home_path.joinpath('results/debug')
+    og_name = settings.audio.audio_filename
 
     ## INITIALIZATION
     print('Initializing RELAX...')
@@ -46,6 +52,8 @@ def run_batched_relax(home_path: Path, settings: AllSettings):
     audio_path = audio_folder.joinpath(settings.audio.audio_filename)
 
     audio, cpx_spec, spec_db, spec_shape = prepare_audio(audio_path)
+
+    plot_and_save_spec(spec_db[0, :, :], debug_folder, og_name)
 
     # II. BEATS model
     beats_folder = home_path.joinpath('beats')
@@ -69,10 +77,14 @@ def run_batched_relax(home_path: Path, settings: AllSettings):
 
         # 1. Create masks
         masks = create_random_masks(spec_shape=spec_shape, n_masks=settings.relax.num_of_masks, **vars(settings.masking))
-        mask_weight_mx += torch.mean(masks.float(), dim=0)
+        mask_weight_mx += torch.mean(masks.float(), dim=0)      
         # 2. Apply masks
-        masked_cpx_specs = apply_masks(cpx_spec, masks)
+        masked_cpx_specs = apply_advanced_masks(cpx_spec, masks)
         masked_audio_signals = inverse_complex_spectrogram(masked_cpx_specs)
+        # +++ Debug files
+        if b == 0:
+            selected_idx = plot_and_save_masks(masks, debug_folder, og_name)
+            plot_and_save_masked_audio(masked_audio_signals, selected_idx, debug_folder, og_name)
         # 3. Extract features
         _, _, h_masked, test_masked = beats_model.extract_features(masked_audio_signals)
         # 4. Compute similarity
